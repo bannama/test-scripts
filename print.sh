6457998af1d84c39543ca24afd9dee2b3159f7dc
Bhaskar Annamalai - bannama <bannama@L-SB8X2BPG8W-M.local>
2016-09-01 07:29:46 -0700

    env

--- print.sh
+++ print.sh
@@ -2,6 +2,6 @@
 source /etc/profile.d/oneops.conf
 
 echo "----------- Running print script -------------"
-printenv ONEOPS_CLOUD
+echo $ONEOPS_CLOUD
 echo "Current Version : " $1
 echo "New Version : " $2
