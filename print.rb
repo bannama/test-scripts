8c9395552f6ef87d82dfa7120e43e380255849f3
Bhaskar Annamalai - bannama <bannama@L-SB8X2BPG8W-M.local>
2016-09-01 11:01:23 -0700

    env

---
+++ print.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+
+curr=ARGV[0]
+new=ARGV[1]
+puts curr
+puts new
--- print.sh
+++ print.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+source /etc/profile.d/oneops.sh
+
 echo "----------- Running print script -------------"
+printenv ONEOPS_CLOUD
 echo "Current Version : " $1
 echo "New Version : " $2
